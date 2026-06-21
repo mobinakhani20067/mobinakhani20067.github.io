7f88f476175392b203a50042c2d1abafb4955460
mobinakhani20067 <mobinakhani20067@gmail.com>
2026-06-21 19:54:16 +0330

    Create planner.js

---
+++ js/planner.js
@@ -0,0 +1,8 @@
+function goAddTask() {
+	location.href="Planner -addTask.html";}
+function goTodayTask() {
+	location.href="Planner -todayTask.html";}
+function goWeekly() {
+	location.href="Planner - WeeklyPlan.html";}
+function goClander() {
+	location.href="Planner_clander.html";}
